afcb62ace3db2b2bbdb1d4bcea2a088c22b8db80
bai-lu <vasterlu@gmail.com>
2023-04-19 03:28:32 +0800

    resolve no.4

--- 4.寻找两个正序数组的中位数.go
+++ 4.寻找两个正序数组的中位数.go
@@ -8,10 +8,37 @@ package leetcode
 
 // @lc code=start
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	for i, j := 0, 0; i < len(nums1) || j < len(nums2); i, j = i+1, j+1 {
+	merged := []int{}
+	i, j := 0, 0
+	for i < len(nums1) || j < len(nums2) {
+		if i == len(nums1) {
+			merged = append(merged, nums2[j])
+			j++
+			continue
+		}
 
+		if j == len(nums2) {
+			merged = append(merged, nums1[i])
+			i++
+			continue
+		}
+		if nums1[i] <= nums2[j] {
+			merged = append(merged, nums1[i])
+			i++
+		} else {
+			merged = append(merged, nums2[j])
+			j++
+		}
 	}
-	return 0
+	if len(merged)%2 == 0 {
+		left := len(merged)/2 - 1
+		right := len(merged) / 2
+		return float64(merged[left]+merged[right]) / 2
+
+	} else {
+		mid := len(merged) / 2
+		return float64(merged[mid])
+	}
 
 }
 
---
+++ 4.寻找两个正序数组的中位数_test.go
@@ -0,0 +1,9 @@
+package leetcode
+
+import "testing"
+
+func Test_findMedianSortedArrays(t *testing.T) {
+	mid := findMedianSortedArrays([]int{3, 7, 9}, []int{2, 5, 7})
+	println(mid)
+
+}
